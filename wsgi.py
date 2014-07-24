57c6ca10e13537d7530feacb65b357639570c659
Will Hattingh <will.hattingh@neustar.biz>
2014-07-24 10:34:15 -0400

    Adding back in the error and not found templates

--- wsgi.py
+++ wsgi.py
@@ -14,8 +14,18 @@ urls = (
     '/', 'app.controllers.home.index',
     '/hello/', 'app.controllers.hello.index',
 )
+def notfound():
+    render = web.template.render(config.global_template_path, base='layout', cache=config.get_cache_config(), globals=globals())
+    return web.notfound(render.notfound())
+
+def internalerror():
+    render = web.template.render(config.global_template_path, base='layout', cache=config.get_cache_config(), globals=globals())
+    return web.notfound(render.internalerror())
 
 app = web.application(urls, globals())
+# You can change this to run only under the else condition (above app.wsgifunc()) to show as it's in prod.
+app.notfound = notfound
+app.internalerror = internalerror
 current_dir = os.path.dirname(__file__)
 session = web.session.Session(app, web.session.DiskStore(os.path.join(current_dir, 'sessions')), )
 main = app.wsgifunc()
